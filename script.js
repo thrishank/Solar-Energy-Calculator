eeb598e060070ee020b6048867e8b47a0084403e
kmanish1 <[kmanishreddy11@gmail.com]>
2023-03-03 22:25:45 +0530

    ac

--- script.js
+++ script.js
@@ -1,6 +1,3 @@
-const menu = document.getElementsByClassName("dropdown_menu");
-const bars = document.getElementsByTagName("i");
-
 for (let i = 0; i < bars.length; i++) {
   bars[i].addEventListener("click", function() {
     if (menu[0].style.display === "block") {
