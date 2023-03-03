ea619b12ad6f4f4f228488e5dac6bf44d989ad53
[thrishank] <[thrishankkalluru@gmail.com]>
2023-03-03 22:08:34 +0530

    added Animaations

--- script.js
+++ script.js
@@ -1,3 +1,6 @@
+const menu = document.getElementsByClassName("dropdown_menu")
+const bars = document.getElementsByTagName("i")
+
 for (let i = 0; i < bars.length; i++) {
     bars[i].addEventListener("click", function () {
         if (menu[0].style.display === "block") {
